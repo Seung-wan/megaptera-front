9eea540878a1814393066ec9ba5e3bcb72e9d0b0
Seung-wan <tmddhks0104@gmail.com>
2023-03-10 02:57:40 +0900

    docs: week6 redux

--- external-store/src/components/CounterControl.tsx
+++ external-store/src/components/CounterControl.tsx
@@ -1,27 +1,27 @@
-import useCounterStore from '../hooks/useCounterStore';
+import useDispatch from '../hooks/useDispatch';
+import useSelector from '../hooks/useSelector';
+import { decrease, increase } from '../stores/Store';
 
 export default function CounterControl() {
-  const store = useCounterStore();
+  const dispatch = useDispatch();
 
-  const handleClickIncrease = () => {
-    store.count = store.count + 1;
-    store.publish();
-  };
-
-  const handleClickDecrease = () => {
-    store.count = store.count - 1;
-    store.publish();
-  };
+  const count = useSelector((state) => state.count);
 
   return (
     <div>
-      <p>{store.count}</p>
-      <button type="button" onClick={handleClickIncrease}>
+      <p>{count}</p>
+      <button type="button" onClick={() => dispatch(increase())}>
         Increase
       </button>
+      <button type="button" onClick={() => dispatch(increase(10))}>
+        Increase 10
+      </button>
 
-      <button type="button" onClick={handleClickDecrease}>
+      <button type="button" onClick={() => dispatch(decrease())}>
         Decrease
+      </button>
+      <button type="button" onClick={() => dispatch(decrease(10))}>
+        Decrease 10
       </button>
     </div>
   );
